a5e1c36267397e535d2a4b9ed3fde2fca1474653
Daspoet <cederk2306@gmail.com>
2021-09-24 23:04:19 +0200

    feat(server): add basic handlers

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package goba_server
 
 import (
+	"github.com/gomuddle/goba"
 	"github.com/valyala/fasthttp"
 	"net/http"
 )
@@ -16,4 +17,68 @@ func (s Server) handle(handler handlerFunc) fasthttp.RequestHandler {
 			s.error(ctx, err, http.StatusInternalServerError)
 		}
 	}
+}
+
+// getImage retrieves the image matching the
+// given parameters and sends it to the client.
+func (s Server) getImage() handlerFunc {
+	type response goba.Image
+	return func(ctx *fasthttp.RequestCtx) error {
+		typ, name := pathParameter(ctx, "type"), pathParameter(ctx, "name")
+		image, err := s.goba.FindImage(goba.DatabaseType(typ), name)
+		if err != nil {
+			return err
+		}
+		return s.writeJSON(ctx, response(*image))
+	}
+}
+
+// getAllImages retrieves all images with the
+// given type and sends them to the client.
+func (s Server) getAllImages() handlerFunc {
+	type response []goba.Image
+	return func(ctx *fasthttp.RequestCtx) error {
+		typ := pathParameter(ctx, "type")
+		images, err := s.goba.AllImages(goba.DatabaseType(typ))
+		if err != nil {
+			return err
+		}
+		return s.writeJSON(ctx, response(images))
+	}
+}
+
+// applyImage applies the image matching the given
+// parameters to the corresponding database.
+func (s Server) applyImage() handlerFunc {
+	return func(ctx *fasthttp.RequestCtx) error {
+		typ, name := pathParameter(ctx, "type"), pathParameter(ctx, "name")
+		return s.goba.ApplyImage(goba.DatabaseType(typ), name)
+	}
+}
+
+// createImage creates an image of the database
+// with the given type and sends it to the client.
+func (s Server) createImage() handlerFunc {
+	type response goba.Image
+	return func(ctx *fasthttp.RequestCtx) error {
+		typ := pathParameter(ctx, "type")
+		image, err := s.goba.CreateImage(goba.DatabaseType(typ))
+		if err != nil {
+			return err
+		}
+		return s.writeJSON(ctx, response(*image))
+	}
+}
+
+// deleteImage deletes the image with the given parameters.
+func (s Server) deleteImage() handlerFunc {
+	return func(ctx *fasthttp.RequestCtx) error {
+		typ, name := pathParameter(ctx, "type"), pathParameter(ctx, "name")
+		return s.goba.DeleteImage(goba.DatabaseType(typ), name)
+	}
+}
+
+// pathParameter returns the value associated with the given key in the request's path.
+func pathParameter(ctx *fasthttp.RequestCtx, key string) string {
+	return ctx.UserValue(key).(string)
 }
--- routes.go
+++ routes.go
@@ -2,5 +2,9 @@ package goba_server
 
 // routes configures s's endpoints.
 func (s Server) routes() {
-
+	s.router.GET("/images/{type}/{name}", s.handle(s.authMiddleware(s.getImage())))
+	s.router.GET("/images/{type}", s.handle(s.authMiddleware(s.getAllImages())))
+	s.router.POST("/images/{type}/{name}", s.handle(s.authMiddleware(s.applyImage())))
+	s.router.POST("/images/{type}", s.handle(s.authMiddleware(s.createImage())))
+	s.router.DELETE("/images/{type}/{name}", s.handle(s.authMiddleware(s.deleteImage())))
 }
